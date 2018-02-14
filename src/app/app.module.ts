4ffd5d12a00098e6b28bec3f602518daccdcf052
Krunal <krunallathiya21@gmail.com>
2018-02-14 15:31:25 +0530

    import route changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
-import appRoutes from './routerConfig';
+import { appRoutes } from './routerConfig';
 
 import { CoinService } from './coin.service';
 
